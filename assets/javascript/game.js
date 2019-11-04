862a797041d2e94e17f952e3543bd083dfbe3af6
hycoding51 <yanghuan.bnu@gmail.com>
2019-11-03 21:13:02 -0500

    capture and store any valid key (letters) pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,7 @@ var dictionary = [
 ];
 var lives = 12;
 var wins = 0;
+
 // choose a random word from dictionary
 chosenWord = dictionary[Math.floor(Math.random() * dictionary.length)];
 console.log(chosenWord);
@@ -23,6 +24,52 @@ console.log(chosenWordUpper);
 chosenWordUpperSplit = chosenWordUpper.split("");
 console.log(chosenWordUpperSplit);
 
-// display the word on screen with _ _ _ _ _
-// flag =
-// chosenWordDisplay = chosenWord[flag];
+//   // display the word on screen with _ _ _ _ _
+var flag = Array(chosenWord.length).fill("_");
+console.log(flag.join(" "));
+
+//set up an array to store letters entered by player
+var guessedLetters = [];
+
+// store all possible letters in an array
+var validGuess = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+// when player press any key, store pressed key
+document.onkeydown = function(event) {
+  console.log(event.key);
+  var pressedKey = event.key.toUpperCase();
+  console.log(pressedKey);
+  // if pressed key is a valid letter, the game start
+  if (validGuess.indexOf(pressedKey) !== -1) {
+    var guessTimes = guessedLetters.push(pressedKey);
+    console.log(guessedLetters);
+    console.log(guessedLetters.join(" "));
+  }
+};
